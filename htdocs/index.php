70d7fe1b0584974da3737a35809c7c3655c61c4c
marcoantoniodf0709-debug <marcoantoniodf0709@gmail.com>
2026-06-12 19:06:56 +0000

    alteração do trbalho Marco Davi Ester 2D

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
+
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olá Mundo em PHP</title>
-</head>
 <body>
-    <h1><?php echo "Olá Mundo"; ?></h1>
-</body>
+<body>
+    
+    <h1>Testando if ... else com PHP</h1>
+    <?php
+        $hora = 10; // Altere este valor para testar diferentes horários
+        if ($hora < 12) {
+            echo "<p>Bom dia!</p>";
+        } else {
+            echo "<p>Boa tarde!</p>";
+        }
+    ?>
+</body> 
+
+
+<h1>Testando switch com PHP</h1>
+    <?php
+        $dia = "terça"; // Tente mudar para outro dia da semana
+        switch ($dia) {
+            case "segunda":
+                echo "<p>Hoje é dia de programar!</p>";
+                break;
+            case "terça":
+                echo "<p>Dia de banco de dados!</p>";
+                break;
+            default:
+                echo "<p>Dia de estudar novos conceitos.</p>";
+        }
+    ?> 
+
+
+   <h1>Testando while com PHP</h1>
+    <ul>
+        <?php
+            $contador = 1;
+            while ($contador <= 10) {
+                echo "<li>Item de teste número $contador</li>";
+                $contador++;
+            }
+        ?>
+    </ul> 
+
+</body> 
 </html>
